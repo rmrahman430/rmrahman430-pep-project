01966153c3b9a95de86e62fdb8a2ac38faac422b
rmrahman430 <rayyanmrahman@gmail.com>
2024-06-30 20:42:39 +0000

    Service

---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,25 @@
+package Service;
+
+import Model.Message;
+import DAO.MessageDAO;
+
+import java.util.List;
+
+
+public class MessageService {
+    MessageDAO messageDAO;
+
+
+    public MessageService() {
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+    }
+
+    public List<Message> getAllMessages() {
+        return messageDAO.getAllMessages();
+
+    }
+}
